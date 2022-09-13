e3b7127807cc820bf1e9cab791670b4e9bf43de7
Surya Viswanathan <106607233+SuryaViswanathan04@users.noreply.github.com>
2022-09-13 10:22:25 -0500

    Create hi.sh

---
+++ hi.sh
@@ -0,0 +1,11 @@
+echo "hi"
+date
+hostname
+hostname -i
+mkdir hacking1
+touch hello.txt
+mv hello.txt hacking1
+mkdir scripting
+cp hacking / hello.txt scripting
+rm hacking / hello.txt
+rmdir hacking
